80c42853b3449612f6c6da83a1478efc9f8a8e31
Kaining Feng <kainingf@usc.edu>
2024-03-23 21:51:48 +0000

    part1

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -9,10 +9,34 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 
+int checkPaths(Node* node, bool& equalpath);
 
-bool equalPaths(Node * root)
-{
-    // Add your code below
-
+bool equalPaths(Node* root) {
+  if (root == nullptr){
+    return true; 
+  }
+  bool equalpath = true;
+  checkPaths(root, equalpath);
+  return equalpath;
 }
 
+int checkPaths(Node* node, bool& equalpath) {
+  if (node == nullptr){
+    return 0;
+  }
+  int leftd = checkPaths(node->left, equalpath);
+  int rightd = checkPaths(node->right, equalpath);
+  if (leftd == 0 && rightd > 0){
+    leftd =rightd;
+  }
+  if (leftd > 0 && rightd == 0){
+    rightd = leftd;
+  }
+  if (leftd == rightd) {
+    return leftd + 1;
+  } 
+  else {
+    equalpath = false;
+    return -1;
+  }
+}
